e0dfe7ce91a24e496f9ce70dfd7993557f48e208
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-29 01:11:04 +0530

    Added : Linked Hash Map And It's Test Case

--- src/main/java/com/bridgelabz/HashTable/Main.java
+++ src/main/java/com/bridgelabz/HashTable/Main.java
@@ -2,7 +2,7 @@ package com.bridgelabz.HashTable;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to HashTable");
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to HashTable");
+	}
 }
--- src/test/java/com/bridgelabz/HashTable/TestHashMap.java
+++ src/test/java/com/bridgelabz/HashTable/TestHashMap.java
@@ -1,8 +1,5 @@
 package com.bridgelabz.HashTable;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,5 +21,27 @@ public class TestHashMap {
 		hashTable.linkedList.printNode();
 		System.out.println(frequency);
 		Assert.assertEquals(1, frequency);
+	}
+
+	@Test
+	public void givenASentence_whenWordsAreAddedToList_ShouldReturnParanoidFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because "
+				+ "they keep putting themselves deliberately into paranoid avoidable situations";
+
+		HashMap<String, Integer> linkedHashMap = new HashMap<>();
+		String[] sentenceToWords = sentence.toLowerCase().split(" ");
+		for (String word : sentenceToWords) {
+			Integer value = linkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			linkedHashMap.add(word, value);
+		}
+		int frequency = linkedHashMap.get("paranoid");
+
+		linkedHashMap.linkedList.printNode();
+		System.out.println(frequency);
+		Assert.assertEquals(3, frequency);
 	}
 }
